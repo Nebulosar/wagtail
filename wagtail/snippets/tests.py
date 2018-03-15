e7e55ec96135d2956f41d627e078db0288108c11
h.zeinstra <h.zeinstra@lukkien.com>
2018-03-15 10:46:32 +0100

    added tests

--- wagtail/snippets/tests.py
+++ wagtail/snippets/tests.py
@@ -182,7 +182,10 @@ class TestSnippetCreateView(TestCase, WagtailTestUtils):
     def test_create_invalid(self):
         response = self.post(post_data={'foo': 'bar'})
         self.assertContains(response, "The snippet could not be created due to errors.")
-        self.assertContains(response, "This field is required.")
+        # the error should only appear once: against the field, not in the header message
+        self.assertContains(response, """<p class="error-message"><span>This field is required.</span></p>""",
+                            count=1, html=True)
+        self.assertContains(response, "This field is required", count=1)
 
     def test_create(self):
         response = self.post(post_data={'text': 'test_advert',
@@ -264,7 +267,10 @@ class TestSnippetEditView(BaseTestSnippetEditView):
     def test_edit_invalid(self):
         response = self.post(post_data={'foo': 'bar'})
         self.assertContains(response, "The snippet could not be saved due to errors.")
-        self.assertContains(response, "This field is required.")
+        # the error should only appear once: against the field, not in the header message
+        self.assertContains(response, """<p class="error-message"><span>This field is required.</span></p>""",
+                            count=1, html=True)
+        self.assertContains(response, "This field is required", count=1)
 
     def test_edit(self):
         response = self.post(post_data={'text': 'edited_test_advert',
